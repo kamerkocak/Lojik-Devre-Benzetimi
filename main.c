fd59cca634e90f406ddfb7f19ae7e9da97e3e8fa
Kamer Koçak <31200856+kamerkocak@users.noreply.github.com>
2018-11-14 17:05:42 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <string.h>
+#include <time.h>
 
 
- //.kapi <kapý tipi> <giriþ sayýsý> <çýkýþ> <giriþ listesi> <kapýnýn giriþlere cevap verme süresi>
+ //.kapi <kapy tipi> <giri? sayysy> <çyky?> <giri? listesi> <kapynyn giri?lere cevap verme süresi>
 struct deger_devres1{
     char c1;
     char tab[5];
@@ -69,18 +70,23 @@ struct devres5{
     char girisch2;
 };
 
-int logTime(int *g)
-{
+int logTime(char g[3])
+{	
+ 
+ 	
+	
   FILE *timelog;
-  timelog= fopen("time.txt","a");
+  timelog= fopen("timeabc.txt","a+");
+  
+  fputs(g, timelog);
   time_t result;
   result = time(NULL);
   struct tm* brokentime = localtime(&result);
-  fprintf(timelog,asctime(brokentime));             //ya cozumunu bulacaksýn ya da tek tek fonksiyon olusturacaksýn
- // fprintf(timelog, g);
-
-  fputs(g, timelog);
+  fputs("\t",timelog);
+  fprintf(timelog,asctime(brokentime));
+  fputs("\n\n\n\n",timelog);
   fclose(timelog);
+
 
 }
 
@@ -88,30 +94,21 @@ int logTime(int *g)
 
 int main()
 {
-    int giris,i;
+    int i;
+    char giris[3];
     printf("Lojik Devre Benzetimi Programina Hos Geldiniz...\n\n");
-    printf("Program ile ne yapmak istiyorsaniz lutfen komutununun bulundugu sayiyi giriniz.\n\n");
-    printf("1)Y    2)I     3)H\n\n4)L    5)S     6)G\n\n7)G*   8)K     9)C\n\n");
+    printf("Program ile ne yapmak istiyorsaniz lutfen komutunu giriniz.\n\n");
+    printf("\n\t  KOMUTLAR\t\t\t\n\n");
+    printf("	Y    I     H\n\n	L    S     G\n\n	G*   K     C\n\n");
 
     up: printf(">");
-    scanf("%d",&giris);
-    logTime(&giris);
-
-    char girisler[7];
-    char kapi1[5];
-    char kapi2[5];
-    char kapi3[5];
-    char a='a';
-    char b='b';
-    char c='c';
-    char d='d';
-    char e='e';
-    char f='f';
+    scanf("%s",&giris);
+    logTime(giris);
 
 
-    if(giris==1)
+    if(giris[0]=='y'||giris[0]=='Y')
     {
-        //TUM DOSYA ICERÝGÝNÝ YAZDIRMAK
+        //TUM DOSYA ICERIGINI YAZDIRMAK
         char c[1000];
         FILE *devre;
         if ((devre = fopen("devre.txt", "r")) == NULL)
@@ -121,14 +118,14 @@ int main()
         }
 
             // reads text until newline    https://www.programiz.com/c-programming/examples/read-file
-        fscanf(devre,"%[^\0]", c); //^\n olursa newline'a kadar okur, \0 oldugunda sonlandýrma karakterine kadar okuyor.
+        fscanf(devre,"%[^\0]", c); //^\n olursa newline'a kadar okur, \0 oldugunda sonlandyrma karakterine kadar okuyor.
 
         printf("%s\n\n", c);
         fclose(devre);
 
         goto up;
     }
-    if(giris==2)
+    if(giris[0]=='i'||giris[0]=='I')
     {
         FILE *deger_devre;
         if ((deger_devre = fopen("deger_devre.txt", "r")) == NULL)
@@ -146,19 +143,19 @@ int main()
         fputs("f    0\n",deger_devre);
 
         deger_devre=fopen("deger_devre.txt","r");
-        k.c1=getc(deger_devre);
-        fgets(k.tab, 5, deger_devre);
-        k.deger1=getc(deger_devre);
+        fscanf(deger_devre,"%c",k.c1);//ÇALIŞMIYOR
+        //fgets(deger_devre,"%s",k.tab);
+        //fscanf(deger_devre,"%d",k.deger1);
 
-        printf("%c",k.c1);
-        printf("%c",k.deger1);
+        printf("%c",k.c1);//CALIŞMIYOR.
+        //printf("%d",k.deger1);
         
 
        // goto up;
 
     }
 
-    if(giris==3)
+    if(giris[0]=='h'||giris[0]=='H')
     {
 
         int num=0,sayac=0;
@@ -179,41 +176,39 @@ int main()
 
 
 
-        /*ptr = (char*) malloc(num * sizeof(char));  // malloc hallet        //ayný cozumu 4. maddede de yap
-
+        /*ptr = (char*) malloc(num * sizeof(char));  // malloc hallet        //ayny cozumu 4. maddede de yap
         for(i = 0; i < num; ++i)
         {  printf("lojik-1 yapmak istediginiz degeri/degerleri giriniz: ");
             scanf("%d", ptr + i);
-
         }
         free(ptr);
         //girilen degeri/degerleri 1 yapacak*/
         goto up;
     }
 
-    if(giris==4)
+    if(giris[0]=='l'||giris[0]=='L')
     {
 
         //girilen degeri/degerleri 0 yapacak
         goto up;
     }
 
-    if(giris==5)
+    if(giris[0]=='s'||giris[0]=='S')
     {
 
-        //devreyi simule eder(calýstýrýr?)
+        //devreyi simule eder(calistirir)
         goto up;
     }
 
-    if(giris==6)
+    if(giris[0]=='g'||giris[0]=='G')
     {
 
 
-        //girilen uclarýn degerlerini konsolda gosterir
+        //girilen uclaryn degerlerini konsolda gosterir
         goto up;
     }
 
-    if(giris==7)
+    if(giris[0]=='g'||giris[0]=='G' && giris[1]=='*')   //BURADA BIR SIKINTIVAR AMA COZCEM
     {   printf("Tum uclarin guncel seviyesi asagida listelenmistir...\n\n");
         char gyildiz[1000];
         FILE *deger;
@@ -224,26 +219,26 @@ int main()
         }
 
             // reads text until newline    https://www.programiz.com/c-programming/examples/read-file
-        fscanf(deger,"%[^\0]", gyildiz); //^\n olursa newline'a kadar okur, \0 oldugunda sonlandýrma karakterine kadar okuyor.
+        fscanf(deger,"%[^\0]", gyildiz); //^\n olursa newline'a kadar okur, \0 oldugunda sonlandyrma karakterine kadar okuyor.
 
         printf("%s\n\n", gyildiz);
         fclose(deger);
 
 
-        //tüm uclarýn degerlerini konsolda gosterir
+        //tüm uclaryn degerlerini konsolda gosterir
         goto up;
     }
 
-    if(giris==8)
+    if(giris[0]=='k'||giris[0]=='K')
     {
-        //komut.txt icindeki komutlarý konsoldan icra eder
+        //komut.txt icindeki komutlary konsoldan icra eder
         goto up;
     }
 
-    if(giris==9)
+    if(giris[0]=='c'||giris[0]=='C')
     {
         //benzetimden cikis yapar
-        //bu komut girilene kadar olan tum komutlarý log dosyasýna tarih saat dakika olarak kaydet
+        //bu komut girilene kadar olan tum komutlary log dosyasyna tarih saat dakika olarak kaydet
 
         printf("\nBenzetimden cikiliyor...\n");
         exit(0);
@@ -252,11 +247,5 @@ int main()
 
 
 
-
-
-
-
     return 0;
 }
-
- 
